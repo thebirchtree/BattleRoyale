1779d21183f4e479a49ece0f74aacd3fef4bb0b5
Thomas Huster <tehuster@gmail.com>
2017-11-17 16:47:21 +0100

    RPC_GunFX

---
+++ FPS Multiplayer/Assets/Scripts/FXManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXManager : MonoBehaviour {
+
+	[PunRPC]
+	void BulletFX( Vector3 startPos, Vector3 endPos){
+		 Debug.Log("Bulet FX!");
+	}
+}
--- FPS Multiplayer/Assets/Scripts/Shooting.cs
+++ FPS Multiplayer/Assets/Scripts/Shooting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-
+    FXManager fxManager;
     public float damage = 5f;
     public float impact = 100f;
     public float range = 250f;
@@ -19,6 +19,10 @@ public class Shooting : MonoBehaviour
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
+        fxManager = GameObject.FindObjectOfType<FXManager>();
+        if(fxManager == null){
+            Debug.Log("Could not find fxManager..");
+        }
     }
     void Update()
     {
@@ -34,17 +38,22 @@ public class Shooting : MonoBehaviour
     {       
        RaycastHit hit;  
 
-        if( Physics.Raycast( Camera.main.transform.position, Camera.main.transform.forward, out hit, range, Mask ) ){
+        if( Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, range, Mask ) ){
             Health h = hit.collider.GetComponent<Health>();
-              Debug.Log(hit.transform.parent.name);
-             if(h == null && hit.transform.parent){          
+              
+            if(h == null && hit.transform.parent){          
                 
                  h = hit.transform.parent.GetComponent<Health>();
-             }         
+            }         
 
             if (h != null){
                 h.GetComponent<PhotonView>().RPC ("takeDamage", PhotonTargets.AllBuffered, damage);  //h.takeDamage (damage);               
                 Debug.Log("Health remainig: " + h.getHealth());
+            }
+
+            if (fxManager != null) {
+                Vector3 maxRangePoint = Camera.main.transform.position + (Camera.main.transform.forward * 100f);
+                fxManager.GetComponent<PhotonView>().RPC ("BulletFX", PhotonTargets.All,Camera.main.transform.position,maxRangePoint);
             }
             // if we want to use add force to the target, not using now because of MP.
             // if (hit.rigidbody != null){ 
@@ -53,6 +62,10 @@ public class Shooting : MonoBehaviour
             audioSource.PlayOneShot(gunShot, 0.5F);
             GameObject hitImpactFX = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(hitImpactFX, 1f);
+        }else{
+             if (fxManager != null) {
+                fxManager.GetComponent<PhotonView>().RPC ("BulletFX", PhotonTargets.All,Camera.main.transform.position,hit.point);
+            }
         }        
 
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * range, Color.green, 0, true);
